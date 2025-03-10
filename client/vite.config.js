5ee0c50c6fea4f0d1657391c5c195cad0d787849
Darshan Naik <naikdarshan1608@gmail.com>
2025-03-10 11:01:31 +0530

    Commit Changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {},
+// vite.config.js
+export default {
+  build: {
+    rollupOptions: {
+      external: ['@safe-globalThis/safe-ethers-adapters'],
+    },
   },
-});
+};
+
+// vite.config.js
